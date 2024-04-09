e46e24775ecf9efc57c5f86b9e1b6053537054d0
Vedant Tilwani <vedant-pawan.tilwani@hpe.com>
2024-04-09 15:43:57 +0530

    updated resume and experience

--- src/Experience/ExperienceDetails/CompanyDetails.js
+++ src/Experience/ExperienceDetails/CompanyDetails.js
@@ -21,9 +21,10 @@ export default function CompanyDetails({companyID, colorSelection, colorCombinat
                     <div className="experience-accomplishments">
                         Aruba is the global leader in wired, wireless, and SD-WAN solutions that use AI to automate and secure the network from edge-to-cloud.
                         <ul>
-                            <li><div>I build & maintain their cloud-based networking solution website - Aruba Central</div></li>
-                            <li><div>Built a ChatGPT model to generate answers to users' networking queries</div></li>
-                            <li><div>Worked on a BERT sementic search classifier</div></li>
+                            <li><div>I build & maintain their cloud-based networking solution website - Aruba Central.</div></li>
+                            <li><div>Designed and implemented a high-volume search bar that processes over 9,000+ monthly queries, delivering user device information, relevant in-page navigation, and summarized documentation with citations using Large Language Models (LLMs).</div></li>
+                            <li><div>Integrated Captive Portals for seamless client authentication within Aruba Central while strategically executing and optimizing data flows for enhanced user authentication and access control functions across various cloud providers (Google Azure, AWS and Okta).</div></li>
+                            <li><div>Led the migration of containerized applications from x86 to ARM processors.</div></li>
                         </ul> 
                     </div>
                 </>
@@ -47,8 +48,8 @@ export default function CompanyDetails({companyID, colorSelection, colorCombinat
                     <div className="experience-accomplishments">
                         Cloudesign is a global IT Consulting, IT Outsourcing and Software Engineering firm in Mumbai.
                         <ul>
-                            <li><div>Made an Attendance Bot on Slack</div></li>
-                            <li><div>Graphically presented reports and analytics</div></li>
+                            <li><div>Developed an Attendance Bot on Slack streamlining attendance and tracking and managing the process for the team.</div></li>
+                            <li><div>Utilized the ELK Stack to automate the generation of visually enriched reports and analytics for the client, enhancing visualization and streamlining data processing.</div></li>
                         </ul> 
                     </div>
                 </>
